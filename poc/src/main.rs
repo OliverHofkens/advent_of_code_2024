9d8c7595be74390f49f3438639739a1452861bdb
Oliver Hofkens <oliver@gorilla.co>
2024-11-12 15:28:48 +0100

    feat: solve 2023 day 1 as PoC

--- poc/src/main.rs
+++ poc/src/main.rs
@@ -2,7 +2,6 @@
 #![no_std]
 #![no_main]
 use aoc_common::io;
-use embedded_io::Read;
 use esp_backtrace as _;
 use esp_hal::usb_serial_jtag::UsbSerialJtag;
 use esp_hal::{delay::Delay, prelude::*};
@@ -17,18 +16,26 @@ fn main() -> ! {
     let mut usb_serial = UsbSerialJtag::new(peripherals.USB_DEVICE);
     let mut reader = io::LineReader::<64>::new();
 
-    println!("Initialized");
+    let mut sum: u32 = 0;
 
     loop {
-        delay.delay(100.millis());
+        delay.delay(10.millis());
 
         match reader.read_until_newline(&mut usb_serial) {
             Ok(true) => {
                 let line = reader.line();
-                println!("Read: {:?}", line);
+
+                let mut iter = line.iter().filter_map(|d| (*d as char).to_digit(10));
+                let first_digit = iter.next().unwrap();
+                let last_digit = iter.next_back().unwrap_or(first_digit);
+                let number = first_digit * 10 + last_digit;
+                println!("Number: {}", number);
+                sum += number;
             }
-            Ok(false) => println!("Nothing"),
+            Ok(false) => continue,
             Err(e) => println!("Error reading! {}", e),
         }
+        println!("Result: {}", sum);
+        reader.clear();
     }
 }
