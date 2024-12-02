8686115ab7d29e42c947126d9377fadde736898a
Oliver Hofkens <23633993+OliverHofkens@users.noreply.github.com>
2024-12-02 11:05:47 +0100

    refactor(day_1): clean up code as prep/template for coming days

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -8,60 +8,8 @@ use esp_hal::{delay::Delay, prelude::*};
 use esp_println::println;
 use heapless::Vec;
 
-// Part 1
-
-// #[entry]
-// fn main() -> ! {
-//     #[allow(unused)]
-//     let peripherals = esp_hal::init(esp_hal::Config::default());
-//
-//     let delay = Delay::new();
-//     let mut usb_serial = UsbSerialJtag::new(peripherals.USB_DEVICE);
-//     let mut reader = io::LineReader::<64>::new();
-//
-//     let mut eof: bool = false;
-//
-//     const SIZE: usize = 1000;
-//     let mut left: Vec<i32, SIZE> = Vec::new();
-//     let mut right: Vec<i32, SIZE> = Vec::new();
-//
-//     loop {
-//         delay.delay(1.millis());
-//
-//         match reader.read_until_newline(&mut usb_serial) {
-//             Ok(true) => {
-//                 let line = core::str::from_utf8(reader.line()).unwrap();
-//                 let mut parts = line.split(' ');
-//
-//                 let l = i32::from_str_radix(parts.next().unwrap(), 10).unwrap();
-//                 left.push(l).unwrap();
-//                 let r = i32::from_str_radix(parts.next_back().unwrap(), 10).unwrap();
-//                 right.push(r).unwrap();
-//
-//                 println!("{} {}", l, r);
-//             }
-//             Ok(false) => eof = true,
-//             Err(e) => println!("Error reading!"),
-//         }
-//         reader.clear();
-//
-//         if eof {
-//             let mut sum: i32 = 0;
-//             left.sort_unstable();
-//             right.sort_unstable();
-//
-//             for (l, r) in left.iter().zip(right.iter()) {
-//                 sum += (l - r).abs();
-//             }
-//             println!("Sum of distances: {}", sum);
-//         }
-//     }
-// }
-
-// Part 2
 #[entry]
 fn main() -> ! {
-    #[allow(unused)]
     let peripherals = esp_hal::init(esp_hal::Config::default());
 
     let delay = Delay::new();
@@ -74,7 +22,7 @@ fn main() -> ! {
     let mut left: Vec<i32, SIZE> = Vec::new();
     let mut right: Vec<i32, SIZE> = Vec::new();
 
-    loop {
+    while !eof {
         delay.delay(1.millis());
 
         match reader.read_until_newline(&mut usb_serial) {
@@ -86,22 +34,35 @@ fn main() -> ! {
                 left.push(l).unwrap();
                 let r = i32::from_str_radix(parts.next_back().unwrap(), 10).unwrap();
                 right.push(r).unwrap();
+
+                println!("{} {}", l, r);
             }
             Ok(false) => eof = true,
             Err(e) => println!("Error reading!"),
         }
         reader.clear();
+    }
 
-        if eof {
-            let mut similarity: i32 = 0;
+    left.sort_unstable();
+    right.sort_unstable();
 
-            for num in &left {
-                let count = right.iter().filter(|x| *x == num).count() as i32;
+    // Part 1
+    let mut sum: i32 = 0;
+    for (l, r) in left.iter().zip(right.iter()) {
+        sum += (l - r).abs();
+    }
+    println!("Sum of distances: {}", sum);
 
-                similarity += num * count;
-            }
+    // Part 2
+    let mut similarity: i32 = 0;
+    for num in &left {
+        let count = right.iter().filter(|x| *x == num).count() as i32;
+        similarity += num * count;
+    }
+    println!("Total similarity: {}", similarity);
 
-            println!("Total similarity: {}", similarity);
-        }
+    println!("<EOT>");
+    loop {
+        delay.delay(100.millis());
     }
 }
