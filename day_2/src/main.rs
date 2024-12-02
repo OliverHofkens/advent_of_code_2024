54a304e9b6bd3e16c7ab823015424b2ef014a2b5
Oliver Hofkens <23633993+OliverHofkens@users.noreply.github.com>
2024-12-02 18:51:50 +0100

    feat: solve day 2 part 2

--- day_2/src/main.rs
+++ day_2/src/main.rs
@@ -8,6 +8,9 @@ use esp_hal::{delay::Delay, prelude::*};
 use esp_println::println;
 use heapless::Vec;
 
+// Part 1: false, Part 2: true
+const PROBLEM_DAMPENER: bool = true;
+
 #[entry]
 fn main() -> ! {
     let peripherals = esp_hal::init(esp_hal::Config::default());
@@ -20,7 +23,7 @@ fn main() -> ! {
 
     let mut safe: usize = 0;
 
-    'outer: while !eof {
+    while !eof {
         delay.delay(1.millis());
         reader.clear();
 
@@ -32,30 +35,45 @@ fn main() -> ! {
                     .map(|x| i32::from_str_radix(x, 10).unwrap())
                     .collect();
 
-                let mut dir = 0;
-                for window in numbers.windows(2) {
-                    match window[1] - window[0] {
-                        x if x > 0 && x <= 3 && dir >= 0 => dir = 1,
-                        x if x < 0 && x >= -3 && dir <= 0 => dir = -1,
-                        _ => {
-                            println!("{} is unsafe at {:?}", line, window);
-                            continue 'outer;
+                match unsafe_at(&numbers) {
+                    None => safe += 1,
+                    Some(ix) if PROBLEM_DAMPENER => {
+                        for rem in ix.saturating_sub(1)..=ix.saturating_add(1) {
+                            // Remove x and try again:
+                            let mut retry = numbers.clone();
+                            retry.remove(rem);
+                            if let None = unsafe_at(&retry) {
+                                safe += 1;
+                                println!("Problem dampened: {}", line);
+                                break;
+                            }
                         }
-                    };
+                    }
+                    _ => continue,
                 }
-                println!("{} is safe", line);
-                safe += 1;
             }
             Ok(false) => eof = true,
-            Err(e) => println!("Error reading!"),
+            Err(e) => println!("Error reading! {}", e),
         }
     }
 
-    // Part 1
     println!("Safe: {}", safe);
 
     println!("<EOT>");
     loop {
         delay.delay(100.millis());
     }
+}
+
+fn unsafe_at(levels: &[i32]) -> Option<usize> {
+    let mut dir = 0;
+
+    for (i, window) in levels.windows(2).enumerate() {
+        match window[1] - window[0] {
+            x if x > 0 && x <= 3 && dir >= 0 => dir = 1,
+            x if x < 0 && x >= -3 && dir <= 0 => dir = -1,
+            _ => return Some(i),
+        };
+    }
+    None
 }
