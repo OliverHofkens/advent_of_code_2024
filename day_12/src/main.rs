a4aaa86ebb4433d2066c3913d2fc47d263b8ad8e
Oliver Hofkens <23633993+OliverHofkens@users.noreply.github.com>
2024-12-14 16:21:03 +0100

    feat: solve day 12 part 2 (with help)

--- day_12/src/main.rs
+++ day_12/src/main.rs
@@ -39,7 +39,12 @@ fn main() -> ! {
         }
     }
 
-    let costs = calculate_costs(&mut map);
+    // Part 1
+    // let costs = calculate_costs(&mut map, Costing::PERIMETER);
+    // println!("Cost: {}", costs);
+
+    // Part 2
+    let costs = calculate_costs(&mut map, &Costing::SIDES);
     println!("Cost: {}", costs);
 
     println!("<EOT>");
@@ -48,15 +53,20 @@ fn main() -> ! {
     }
 }
 
-fn calculate_costs(map: &mut Map) -> u64 {
+enum Costing {
+    PERIMETER,
+    SIDES,
+}
+
+fn calculate_costs(map: &mut Map, costing: &Costing) -> u64 {
     let mut sum: u64 = 0;
-    while let Some((area, perim)) = boom_area(map) {
+    while let Some((area, perim)) = boom_area(map, costing) {
         sum += area * perim;
     }
     sum
 }
 
-fn boom_area(map: &mut Map) -> Option<(u64, u64)> {
+fn boom_area(map: &mut Map, costing: &Costing) -> Option<(u64, u64)> {
     // Find a starting point
     let mut area_start: Option<Coord> = None;
     for (y, row) in map.iter().enumerate() {
@@ -72,18 +82,19 @@ fn boom_area(map: &mut Map) -> Option<(u64, u64)> {
             let mut seen = SeenCache::new();
             let marker = map[y as usize][x as usize];
 
-            let (area, perim) = scan(map, &(x, y), marker, &mut seen);
+            let (area, perim) = scan(map, &(x, y), marker, costing, &mut seen);
 
             for (x, y) in seen.iter() {
                 map[*y as usize][*x as usize] = MK_SEEN;
             }
+            println!("{} @ ({x}, {y}): {area}A * {perim}C", marker as char);
 
             Some((area, perim))
         }
     }
 }
 
-fn scan(map: &Map, pos: &Coord, marker: u8, seen: &mut SeenCache) -> (u64, u64) {
+fn scan(map: &Map, pos: &Coord, marker: u8, costing: &Costing, seen: &mut SeenCache) -> (u64, u64) {
     seen.insert(*pos).unwrap();
 
     let xsize = map[0].len() as u8;
@@ -91,23 +102,80 @@ fn scan(map: &Map, pos: &Coord, marker: u8, seen: &mut SeenCache) -> (u64, u64) 
 
     let mut area = 1;
     let mut perim = 0;
+    let mut sides: u64 = 0;
 
-    for (dx, dy) in [(0, 1), (1, 0), (0, -1), (-1, 0)] {
-        let nx: Option<u8> = (pos.0 as i16 + dx).try_into().ok();
-        let ny: Option<u8> = (pos.1 as i16 + dy).try_into().ok();
+    let mut is_diff = [false; 9];
+    let mut i = 0;
 
-        match (nx, ny) {
-            (None, _) | (_, None) => perim += 1,
-            (Some(nx), Some(ny)) if nx >= xsize || ny >= ysize => perim += 1,
-            (Some(nx), Some(ny)) if map[ny as usize][nx as usize] != marker => perim += 1,
-            (Some(nx), Some(ny)) if seen.contains(&(nx, ny)) => (),
-            (Some(nx), Some(ny)) => {
-                let (new_area, new_perim) = scan(map, &(nx, ny), marker, seen);
-                area += new_area;
-                perim += new_perim;
+    for dy in -1..=1 {
+        for dx in -1..=1 {
+            if dx == 0 && dy == 0 {
+                i += 1;
+                continue;
             }
+
+            let nx: Option<u8> = (pos.0 as i16 + dx).try_into().ok();
+            let ny: Option<u8> = (pos.1 as i16 + dy).try_into().ok();
+
+            match (nx, ny) {
+                (None, _) | (_, None) => is_diff[i] = true,
+                (Some(nx), Some(ny)) if nx >= xsize || ny >= ysize => is_diff[i] = true,
+                (Some(nx), Some(ny)) if map[ny as usize][nx as usize] != marker => {
+                    is_diff[i] = true
+                }
+                (Some(nx), Some(ny)) if seen.contains(&(nx, ny)) => (),
+                (Some(nx), Some(ny)) if dx == 0 || dy == 0 => {
+                    // Orthogonals
+                    let (new_area, new_perim) = scan(map, &(nx, ny), marker, costing, seen);
+                    area += new_area;
+                    match costing {
+                        Costing::PERIMETER => perim += new_perim,
+                        Costing::SIDES => sides += new_perim,
+                    }
+                }
+                _ => {
+                    // Diagonal of same marker, don't do anything.
+                }
+            }
+
+            i += 1;
         }
     }
+    match costing {
+        Costing::PERIMETER => {
+            perim += is_diff
+                .iter()
+                .enumerate()
+                .filter_map(|(i, x)| match i % 2 {
+                    1 => Some(*x as u8),
+                    _ => None,
+                })
+                .sum::<u8>() as u64;
+            (area, perim)
+        }
+        Costing::SIDES => {
+            // Paraphrased from Reddit because fuck this
+            let [NW, N, NE, W, _, E, SW, S, SE] = is_diff;
 
-    (area, perim)
+            let corners = [
+                !N && !W && NW,
+                !N && !E && NE,
+                !S && !W && SW,
+                !S && !E && SE,
+                N && W,
+                N && E,
+                S && W,
+                S && E,
+            ]
+            .iter()
+            .map(|b| *b as u8)
+            .sum::<u8>() as u64;
+
+            // println!(
+            //     "[{}] @ {:?}: {corners}C, ({:?})",
+            //     marker as char, pos, is_diff
+            // );
+            (area, sides + corners)
+        }
+    }
 }
