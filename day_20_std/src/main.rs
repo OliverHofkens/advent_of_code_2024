c331fa23185e660d0dd519d0afdcc721b7197e9b
Oliver Hofkens <23633993+OliverHofkens@users.noreply.github.com>
2024-12-29 09:36:23 +0100

    feat: solve day 20 part 2

--- day_20_std/src/main.rs
+++ day_20_std/src/main.rs
@@ -1,12 +1,18 @@
 use heapless::binary_heap::{BinaryHeap, Min};
-use heapless::{Entry, FnvIndexMap, Vec};
+use heapless::Vec;
+use std::collections::hash_map::Entry;
+use std::collections::HashMap;
 use std::env;
 use std::fs;
 
+const CHEAT_LENGTH: i16 = 20;
 const MAP_SIZE: usize = 141;
 type Map = Vec<Vec<bool, MAP_SIZE>, MAP_SIZE>;
 type Pos = (i16, i16);
-type CheatMap = FnvIndexMap<u64, Vec<Pos, 1024>, 1024>;
+type Distances = [[u64; MAP_SIZE]; MAP_SIZE];
+type CheatMap = HashMap<u64, Vec<Pos, 65_536>>;
+// Stack overflow:
+// type CheatMap = FnvIndexMap<u64, Vec<Pos, 512>, 4096>;
 
 fn main() {
     let mut map = Map::new();
@@ -38,36 +44,40 @@ fn main() {
 
     println!("Start @ {:?}, End @ {:?}", start, end);
 
-    let base_distance = dijkstra(&map, start, end).unwrap();
+    let (base_distance, distances) = dijkstra(&map, start, end).unwrap();
     println!("Base distance: {}", base_distance);
 
-    let walls = list_possible_cheat_positions(&map);
+    let roads = list_possible_cheat_positions(&map);
     let mut cheats = CheatMap::new();
 
-    for (x, y) in walls {
-        // Remove the wall:
-        map[y as usize][x as usize] = true;
+    for pos in roads {
+        let cheat_zone = cheat_radius(&map, &pos, CHEAT_LENGTH);
 
-        let new_distance = dijkstra(&map, start, end).unwrap();
-        let saving = base_distance - new_distance;
-        if saving > 0 {
-            match cheats.entry(saving) {
-                Entry::Vacant(v) => {
-                    let mut cheat_positions = Vec::new();
-                    cheat_positions.push((x as i16, y as i16)).unwrap();
-                    v.insert(cheat_positions).unwrap();
-                }
-                Entry::Occupied(mut v) => {
-                    v.get_mut().push((x as i16, y as i16)).unwrap();
+        for cheat_end in cheat_zone {
+            let start_cost = distances[pos.1 as usize][pos.0 as usize];
+            let end_cost = distances[cheat_end.1 as usize][cheat_end.0 as usize];
+            let cheat_cost = taxicab_dist(&pos, &cheat_end);
+
+            if let Some(saving) = end_cost.checked_sub(start_cost + cheat_cost) {
+                // println!("From {:?} to {:?} saves {saving}", pos, cheat_end);
+
+                if saving > 0 {
+                    match cheats.entry(saving) {
+                        Entry::Vacant(v) => {
+                            let mut cheat_positions = Vec::new();
+                            cheat_positions.push(pos).unwrap();
+                            v.insert(cheat_positions);
+                        }
+                        Entry::Occupied(mut v) => {
+                            v.get_mut().push(pos).unwrap();
+                        }
+                    }
                 }
             }
         }
-
-        // Place back the wall:
-        map[y as usize][x as usize] = false;
     }
 
-    println!("Cheats: {:?}", cheats);
+    // println!("Cheats: {:?}", cheats);
 
     let p1: usize = cheats
         .iter()
@@ -114,10 +124,10 @@ impl PartialOrd for Node {
     }
 }
 
-fn dijkstra(map: &Map, start: Pos, end: Pos) -> Option<u64> {
+fn dijkstra(map: &Map, start: Pos, end: Pos) -> Option<(u64, Distances)> {
     let mut q: BinaryHeap<Node, Min, 1024> = BinaryHeap::new();
     let mut processed = [[false; MAP_SIZE]; MAP_SIZE];
-    let mut distance = [[u64::MAX; MAP_SIZE]; MAP_SIZE];
+    let mut distance: Distances = [[u64::MAX; MAP_SIZE]; MAP_SIZE];
 
     distance[start.1 as usize][start.0 as usize] = 0;
     q.push(Node {
@@ -170,13 +180,13 @@ fn dijkstra(map: &Map, start: Pos, end: Pos) -> Option<u64> {
     }
 
     if min_end_cost < u64::MAX {
-        Some(min_end_cost)
+        Some((min_end_cost, distance))
     } else {
         None
     }
 }
 
-fn list_possible_cheat_positions(map: &Map) -> Vec<Pos, 16384> {
+fn list_possible_cheat_positions(map: &Map) -> Vec<Pos, 10_000> {
     let mut res = Vec::new();
 
     for (y, row) in map.iter().enumerate() {
@@ -187,7 +197,7 @@ fn list_possible_cheat_positions(map: &Map) -> Vec<Pos, 16384> {
 
         for (x, is_road) in row.iter().enumerate() {
             // Don't check map borders or roads
-            if x == 0 || x == MAP_SIZE - 1 || *is_road {
+            if x == 0 || x == MAP_SIZE - 1 || !*is_road {
                 continue;
             }
 
@@ -196,4 +206,47 @@ fn list_possible_cheat_positions(map: &Map) -> Vec<Pos, 16384> {
     }
 
     res
+}
+
+/// Returns all coordinates within a Taxicab circle centered on
+/// pos with the given radius.
+fn cheat_radius(map: &Map, pos: &Pos, radius: i16) -> Vec<Pos, 512> {
+    //   #
+    //  ###
+    // #####
+    //  ###
+    //   #
+    let mut res = Vec::new();
+
+    for dy in -1 * radius..=radius {
+        let y = pos.1 + dy;
+
+        if y < 0 || y >= MAP_SIZE as i16 {
+            continue;
+        }
+
+        let xrad = radius - dy.abs();
+        for dx in -1 * xrad..=xrad {
+            let x = pos.0 + dx;
+
+            if dx == 0 && dy == 0 {
+                continue;
+            }
+
+            if x < 0 || x >= MAP_SIZE as i16 || !map[y as usize][x as usize] {
+                continue;
+            }
+
+            res.push((x, y)).unwrap();
+        }
+    }
+    // println!("Circle at {:?}: {:?}", pos, res);
+
+    res
+}
+
+fn taxicab_dist(start: &Pos, end: &Pos) -> u64 {
+    let dx = (end.0 - start.0).abs();
+    let dy = (end.1 - start.1).abs();
+    (dx + dy) as u64
 }
